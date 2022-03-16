6dfb54a67420fc031f89fdbb3882934375f03c33
eumali22 <97612829+eumali22@users.noreply.github.com>
2022-03-16 13:35:06 +0800

    auth demo

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb+srv://mlab_eumali:PCea6GctxCNZLe6@cluster0.9v2px.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
